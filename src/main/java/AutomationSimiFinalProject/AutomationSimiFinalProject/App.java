94a637d3cd668904ae9163613673d528b0544054
AyaAbd-alrahman <ayamuneer@yahoo.comcom>
2025-05-03 10:54:24 +0300

    update

---
+++ src/main/java/AutomationSimiFinalProject/AutomationSimiFinalProject/App.java
@@ -0,0 +1,13 @@
+package AutomationSimiFinalProject.AutomationSimiFinalProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
